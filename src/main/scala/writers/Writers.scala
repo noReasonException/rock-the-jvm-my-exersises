19ca3c7dbeb9e8c60771fe341672cc8df7b95607
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-08-23 15:57:38 +0100

    Writers Exersise, naiveSum

--- src/main/scala/writers/Writers.scala
+++ src/main/scala/writers/Writers.scala
@@ -18,7 +18,27 @@ object Writers {
     }yield cu+next
   }
 
+  def naiveSum(n:Int):Int={
+    if(n<=0) 0
+    else{
+      val curr = naiveSum(n-1)
+      println(s"n-1=$curr")
+      curr+n
+    }
+  }
+  def naiveSumWriter(n:Int):Writer[Vector[String],Int]={
+    def recv(n:Writer[Vector[String],Int]):Writer[Vector[String],Int]={
+      if(n.value==0) n.mapWritten(_:+"Reached Zero")
+      else for {
+        na <- recv(n.bimap(_:+s"Calculating ${n.value-1}",_-1))
+        nb <- n.reset
+      }yield nb+na
+    }
+    recv(Writer.apply(Vector(s"Starting ${n}"),n))
+  }
+
   def main(args:Array[String]):Unit={
-    println(countAndLog(10))
+//    println(countAndLog(10))
+    println(naiveSumWriter(6))
   }
 }
