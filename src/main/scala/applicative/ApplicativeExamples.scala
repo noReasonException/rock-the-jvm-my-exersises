01da882f8b21a8af437a98d5aa836047beb504c1
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-10-24 13:23:56 +0100

    exersise construction of semigroupal given applicative

--- src/main/scala/applicative/ApplicativeExamples.scala
+++ src/main/scala/applicative/ApplicativeExamples.scala
@@ -9,6 +9,6 @@ import cats.{Applicative, Semigroup, Semigroupal}
 object ApplicativeExamples {
   //TODO: ThoughtExperiment
   def productWithApplicatives[F[_],A,B](fa:F[A],fb:F[B])(implicit applicativeF:Applicative[F]):F[Tuple2[A,B]] = {
-    Semigroupal.apply[F].product(fa,fb)
+    applicativeF.product(fa,fb)
   }
 }
