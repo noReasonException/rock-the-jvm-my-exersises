7b3adc794890757e4b4d52104faa73552239d108
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-10-24 13:23:26 +0100

    exersise construction of semigroupal given applicative

---
+++ src/main/scala/applicative/ApplicativeExamples.scala
@@ -0,0 +1,14 @@
+package applicative
+import cats.{Applicative, Semigroup, Semigroupal}
+
+
+
+//Applicative is a Functor capable of lifting any A to F[A] (e.g pure Method)
+//Notable example of an applicative that IS NOT AN monad, The Validated[E,A] type
+
+object ApplicativeExamples {
+  //TODO: ThoughtExperiment
+  def productWithApplicatives[F[_],A,B](fa:F[A],fb:F[B])(implicit applicativeF:Applicative[F]):F[Tuple2[A,B]] = {
+    Semigroupal.apply[F].product(fa,fb)
+  }
+}
