b07d00db115cc63896b2be82d73e8df708fee1ad
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-10-24 13:10:31 +0100

    exersise semigroup

--- src/main/scala/semigroups/SemigroupExercise.scala
+++ src/main/scala/semigroups/SemigroupExercise.scala
@@ -4,7 +4,7 @@ import cats.Semigroup
 import cats.implicits.catsSyntaxSemigroup
 
 import java.util.UUID
-
+import cats.implicits._
 object Semigroups {
   case class Expense(id:Long,amount:Double)
 
@@ -12,12 +12,15 @@ object Semigroups {
   def reduceThings[T:Semigroup](list:List[T]):T=list.reduce(_|+|_)
 
 
-  implicit val expenseSemigroup:Semigroup[Expense] = Semigroup.instance[Expense]((l,r)=>Expense(UUID.randomUUID().hashCode(),l.amount+r.amount))
+  implicit val expenseSemigroup:Semigroup[Expense] = Semigroup.instance[Expense]((l,r)=>Expense(Math.max(l.id,r.id),l.amount+r.amount))
 
 
   def main(args: Array[String]): Unit = {
-    val expenses:List[Expense] = Expense(1,12)::Expense(2,34)::Expense(3,230)::Nil
-    println(reduceThings(expenses))
+//    val expenses:List[Expense] = Expense(1,12)::Expense(2,34)::Expense(3,230)::Nil
+//    println(reduceThings(expenses))
+
+    val transformedSemigroup:Semigroup[String] = expenseSemigroup.imap(exp=>exp.id+","+exp.amount)(str=>Expense(str.split(",").head.toLong,str.split(",").tail.head.toDouble))
+    print(transformedSemigroup.combine("1,2","3,4"))
   }
 
 }
