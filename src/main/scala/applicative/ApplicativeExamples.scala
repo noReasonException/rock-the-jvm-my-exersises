587a438423cbf0db10c05fe977ae7903f4ec234c
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-10-25 11:09:46 +0100

    Applicative is a Semigroup

--- src/main/scala/applicative/ApplicativeExamples.scala
+++ src/main/scala/applicative/ApplicativeExamples.scala
@@ -11,4 +11,12 @@ object ApplicativeExamples {
   def productWithApplicatives[F[_],A,B](fa:F[A],fb:F[B])(implicit applicativeF:Applicative[F]):F[Tuple2[A,B]] = {
     applicativeF.product(fa,fb)
   }
+  def main(args: Array[String]): Unit = {
+    val a:Option[Int] = Some(12)
+    val b:Option[String] = Some("Hello world")
+    val applicativeOption = Applicative.apply[Option]
+
+    print(productWithApplicatives(a,b))
+
+  }
 }
